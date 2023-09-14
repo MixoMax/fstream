cd19f3f5f9f2582b4d23fb2738058b1840deff50
Linus Horn <linushorn@gmx.net>
2023-09-14 22:11:26 +0200

    fixed run_server for linux

--- run_server.py
+++ run_server.py
@@ -3,4 +3,9 @@ import os
 port = 80
 expose_external = True
 
-os.system("uvicorn main:app --host " + ("0.0.0.0" if expose_external else "127.0.0.1") + " --port " + str(port))
+#if windows
+if os.name == "nt":
+    os.system("uvicorn main:app --host " + ("0.0.0.0" if expose_external else "127.0.0.1") + " --port " + str(port))
+#if linux
+else:
+    os.system("sudo python3 -m uvicorn main:app --host " + ("0.0.0.0" if expose_external else "127.0.0.1") + " --port " + str(port))
