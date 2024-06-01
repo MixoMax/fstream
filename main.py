e762cc920157f6fec3da3ff225773d19dd61a010
Linus Horn <linushorn@gmx.net>
2024-06-01 20:16:58 +0200

    idk

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles, StaticFiles
 import Levenshtein as lev
 import os
 import json
+import hashlib
 
 from config import DATA_DIR, INDEX_PATH
 
@@ -12,6 +13,7 @@ os.chdir(os.path.dirname(__file__))
 
 app = FastAPI()
 
+session_tokens = []
 
 #streaming video
 #sort of like a plex server
@@ -22,6 +24,35 @@ app = FastAPI()
 # - [ ] upload_video(video) -> upload video to server
 # - [ ] upload_metadata(metadata) -> upload metadata to server
 
+def get_session_token(userid, password):
+    #userid is email or username
+    csv_path = "./data/users.csv"
+    
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    
+    userid = userid.lower()
+    
+    user_is_valid = False
+    with open(csv_path, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            line = line.split(",")
+            if line[0].lower() == userid and line[1] == hashed_password:
+                user_is_valid = True
+                break
+    
+    if user_is_valid:
+        #generate token
+        token = hashlib.sha256(userid.encode()).hexdigest()
+        return token
+    else:
+        return None
+
+def add_user(userid, password):
+    userid = userid.lower()
+    csv_path = "./data/users.csv"
+    with open(csv_path, "a") as f:
+        f.write(f"{userid},{password}\n")
 
 def get_video(id):
     id = str(id)
@@ -37,8 +68,6 @@ def get_video(id):
                 rel_path = os.path.relpath(abs_path)
                 return rel_path
     return None
-
-
 
 def get_id(title):
     index_path = INDEX_PATH
@@ -138,6 +167,25 @@ def read_root():
     html_path = "./static/index.html"
     return FileResponse(html_path)
 
+@app.route("/login")
+def read_session_token(request: Request):
+    user_id = request.args.get("userid")
+    password = request.args.get("password")
+    session_token = get_session_token(user_id, password)
+    if session_token is None:
+        return "Invalid login", 401
+    else:
+        session_tokens.append(session_token)
+        return session_token
+
+@app.route("/register")
+def read_register(request: Request):
+    #/register?userid=userid&password=password
+    user_id = request.args.get("userid")
+    password = request.args.get("password")
+    add_user(user_id, password)
+    return "User added"
+
 @app.get("/static/{path}")
 def read_static(path):
     static_path = "./static/" + path
@@ -149,9 +197,9 @@ def read_video(id):
     if video_path is None:
         return "Video not found", 404
     return FileResponse(video_path)
-        
-    
-    
+
+
+
 @app.get("/movie/{id}")
 def read_movie(id):
     video_url = f"/video/{id}"
@@ -210,8 +258,11 @@ def read_search(q: str, mode: str = "id"):
         return "Invalid mode", 501
 
 @app.get("/search_results")
-def read_search_results(q: str):
-    #/search_results?q=movie+title&mode=id
+def read_search_results(q: str, mode: str = "id", session_token: str = ""):
+    #/search_results?q=movie+title&mode=id&session_token=token
+    
+    if session_token not in session_tokens:
+        return "Invalid session token", 401
     
     results = read_search(q, mode="both-title")
     
